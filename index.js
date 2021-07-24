51f4edb48332b1917673c245d8ed73be4ffd3585
Anthony Kim <fbssaejun@gmail.com>
2021-07-24 04:28:51 +0000

    sums values with recursion

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-
 function sum(fromN, toN) {
-  // Sum all the values from fromN up to toN
+  //If this condition meets true, return current Value
+  if (fromN === toN) {
+  	return fromN;
+  }
+  //If above statement does not meet true, add current value with invoking(looping) the function again with 1 highert number, untill current value becomes 'toN' variable
+  return fromN + sum(++fromN, toN);
 }
+
+console.log(sum(1,4));
 
 module.exports = sum;
